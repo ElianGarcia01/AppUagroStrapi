e2e9cd7a8ee22da4a9f1513fecbe15860a0204ab
Elian Garcia <telljua90@gmail.com>
2024-11-28 15:19:50 -0600

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,22 +1,21 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
-
 module.exports = {
   settings: {
-      cors: {
-          enabled: true,
-          origin: ['http://localhost:3000'],  // Cambia esto al dominio de tu frontend
-      },
+    cors: {
+      enabled: true,
+      origin: '*',  // Cambia esto al dominio de tu frontend
+    },
   },
+  // Configuración de middlewares
+  middleware: [
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    'strapi::cors',  // Este es el middleware de CORS
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ],
 };
-
