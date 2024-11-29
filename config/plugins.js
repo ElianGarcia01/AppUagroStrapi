2f1d723ce78c662059eef249f8dd8043169ea4d2
Elian Garcia <telljua90@gmail.com>
2024-11-29 09:55:40 -0600

    desp

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', 'dB/96FWPBEd3ZadpLqS9p5nylP1saNcxYvzy0hgndNU='), // Reemplaza con tu secreto generado
+      },
+    },
+  });
+  
