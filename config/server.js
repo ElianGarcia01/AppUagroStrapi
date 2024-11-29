4af45be0bf8836db380d337db07c13e7f32a57b0
Elian Garcia <telljua90@gmail.com>
2024-11-29 11:20:39 -0600

    desp

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
   app: {
     keys: ['4b3Gh7T9kLm1vN9z', 'D8rT5qP6vK2wX3rB'], // Claves únicas y seguras
   },
