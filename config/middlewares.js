0dcc9d5dcfdc00d94a3d5599ad96227b4886bdb4
Elian Garcia <telljua90@gmail.com>
2024-11-29 09:41:25 -0600

    desp

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,41 +1,25 @@
 module.exports = [
-  // Middlewares por defecto
-  {
-    name: 'strapi::errors',
-  },
-  {
-    name: 'strapi::security',
-  },
+  // Configuración del middleware de CORS
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
       origin: [
         'https://dgtidweb.uagro.mx', // Dominio principal
-        'https://dgtidweb.uagro.mx/AppUagroNew', // Ruta exacta del frontend
+        'https://dgtidweb.uagro.mx/AppUagroNew', // Ruta específica del frontend
         'http://dgtidweb.uagro.mx:1337', // Dominio del backend con puerto
       ],
     },
   },
-  {
-    name: 'strapi::poweredBy',
-  },
-  {
-    name: 'strapi::logger',
-  },
-  {
-    name: 'strapi::query',
-  },
-  {
-    name: 'strapi::body',
-  },
-  {
-    name: 'strapi::session',
-  },
-  {
-    name: 'strapi::favicon',
-  },
-  {
-    name: 'strapi::public',
-  },
+
+  // Otros middlewares por defecto de Strapi
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
