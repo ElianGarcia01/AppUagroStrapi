e4016d98029fe634ae17d32bea797f612df958ff
Elian Garcia <telljua90@gmail.com>
2024-12-01 17:27:50 -0600

    despliegue

--- config/server.js
+++ config/server.js
@@ -1,7 +1,5 @@
 module.exports = ({ env }) => ({
+  host: env('HOST', '127.0.0.1'),
+  port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://dgtidweb.uagro.mx'),
-  proxy: true,
-  app: {
-    keys: ['4b3Gh7T9kLm1vN9z', 'D8rT5qP6vK2wX3rB'], // Claves únicas y seguras
-  },
 });
