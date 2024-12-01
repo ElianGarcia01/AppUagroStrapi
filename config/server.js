909ce221766c4d4287060091d4bd0229f90707a4
Elian Garcia <telljua90@gmail.com>
2024-12-01 12:34:33 -0600

    cambio de ruta en server

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: 'https://dgtidweb.uagro.mx', // Cambia la URL para usar HTTPS
+  url: env('PUBLIC_URL', 'https://dgtidweb.uagro.mx'),
+  proxy: true,
   app: {
     keys: ['4b3Gh7T9kLm1vN9z', 'D8rT5qP6vK2wX3rB'], // Claves únicas y seguras
   },
